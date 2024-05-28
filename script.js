58e6e1d046d338458639db7d64fc5ac32429a1b6
faiidd11 <164476245+faiidd11@users.noreply.github.com>
2024-05-28 09:21:41 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper(".bebas", {
+    slidesPerView: 4,
+    spaceBetween: 20,
+    slidesPerGroup: 4,
+    loop:true,
+    centeredSlide: true,
+    grabCursor: true,
+    fade: true,
+  });
